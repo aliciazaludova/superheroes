26c193521af0bd9eae6b2a960c2bb695e0313010
Alicia Zaludova <aliciazaludova@gmail.com>
2018-04-03 18:42:07 -0500

    printing now + more styling added

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -1,13 +1,20 @@
 console.log('superheroes');
 
+// have a flow/order
+// printToDom, buildDomString (because you call printToDom inside of it), execute if fails and execute after loads come next in either order, then actual XHR request, then call it
+
 // sets up xhr request. executes. this is where we are starting to request the data.
 const startApplication = () => {
     let myRequest = new XMLHttpRequest();
     myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
     myRequest.addEventListener("error", executeThisCodeIfXHRFails);
-    myRequest.open("GET", "./db/superheroes.json"); // should this be a file path? open this new method, which is a "get" method
+    myRequest.open("GET", "../db/superheroes.json"); // should this be a file path? open this new method, which is a "get" method
     myRequest.send();
 }
+
+function executeThisCodeIfXHRFails() {
+    console.log("something broke");
+} 
 
 // parses xhr response. passes it to buildDomString function. // no ES6 here.  going through data and seeing what it is made of.
 function executeThisCodeAfterFileLoaded () { const data = JSON.parse(this.responseText);  // "this" is the "what" that the object is being performed on OR it's the "data" var because it holds the JSON.parse
@@ -21,20 +28,34 @@ function executeThisCodeAfterFileLoaded () { const data = JSON.parse(this.respon
 const buildDomString = (fancyArray) => {
     let domString = "";
     fancyArray.forEach((superhero) => {
-        domString += `<div class="row"`;
-        domString += `<div class="col-md-4"`;
-        domString += `<div class="panel"`;
-        domString += `<div class="panel-body"`;
-        domString += `<h3>${superhero.id}</h3>`;
-        domString += `<p>${superhero.jobIds}</p>`;
-        domString += `<p>${superhero.gender}</p>`;
-        domString += `<h1>${superhero.name}</h1>`;
-        domString += `<p>${superhero.description}</p>`;
-        domString += `<img class="superhero-image" src="${superhero.image}" alt="">`;
+        // domString += `<div class="row"`;
+        // domString += `<div class="col-md-4"`;
+        // domString += `<div class="panel"`;
+        // domString += `<div class="panel-body"`;
+        // domString += `<h3>${superhero.id}</h3>`;
+        // domString += `<p>${superhero.jobIds}</p>`;
+        // domString += `<p>${superhero.gender}</p>`;
+        // domString += `<h1>${superhero.name}</h1>`;
+        domString += `<div class="col-md-3">`;
+        domString += `<div class="panel">`;
+        domString +=    `<div class="panel-heading">`;
+        domString +=        `<h3 class="panel-title">Panel title</h3>`;
+        domString += `</div>`;
+        domString +=    `<div class="panel-body">`;
+        domString +=        `<img class="charImage" src="${superhero.image}">`;
+        domString +=  `<p class="charDescription">${superhero.description}</p>`;
+        domString +=    `</div>`;
         domString += `</div>`;
         domString += `</div>`;
-        domString += `</div>`;
-        domString += `</div>`;
+        // domString += `<p>${superhero.description}</p>`;
+        // domString += `<img class="superhero-image" src="${superhero.image}" alt="">`;
     })
+    console.log(domString);
     printToDom(domString, "superhero-cards"); // this is just writing it, not executing it
 }
+
+// write printToDom function that takes a string and an id and writes the string to the id
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+}
+startApplication();
